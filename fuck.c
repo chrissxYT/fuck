11d88162177217a154e186e4f0c5daf266036488
chrissx <chrissx@chrissx.ga>
2019-03-26 21:07:40 +0100

    added documentation in the readme, a simple binfuck interpreter implementation and started building libfuck.

---
+++ fuck.c
@@ -0,0 +1,6 @@
+#include "fuck.h"
+
+char *binfuck_decode(char *ptr, int len);
+char *binfuck_encode(char *ptr, int len);
+char *asmfuck_decode(char *ptr, int len);
+char *asmfuck_encode(char *ptr, int len);
